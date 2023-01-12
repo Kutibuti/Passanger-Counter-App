236bc27c4e26476f8a3c4da48fab9480ac4fe1e1
Ömer Faruk Ayaz <omerfayaz166@gmail.com>
2023-01-12 15:25:05 +0300

    First try

---
+++ index.js
@@ -0,0 +1,23 @@
+let peopleamount = 0
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+function increment() {
+    peopleamount += 1
+    countEl.textContent = peopleamount
+}
+
+function decrement() {
+    peopleamount -= 1
+    countEl.textContent = peopleamount
+}
+
+function save() {
+    let countanddash = peopleamount + " - "
+    console.log(countEl.textContent)
+    saveEl.textContent+= countanddash
+    peopleamount = 0
+    countEl.textContent = peopleamount
+}
+ 
+//12.31 de kaldim
